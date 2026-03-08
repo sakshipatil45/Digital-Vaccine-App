18ffcbb81aaaf3bc6961f91d162cefb3a721062c
Sakshi Patil <sakshicpatil2006@gmail.com>
2026-03-08 18:29:15 +0530

    Update BASE_URL to local IP for physical device connection

--- app/src/main/java/com/example/digitalvaccineapp/network/RetrofitClient.java
+++ app/src/main/java/com/example/digitalvaccineapp/network/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://10.0.2.2:5000/"; // Localhost for Android Emulator
+    private static final String BASE_URL = "http://10.95.27.238:5000/"; // Local IP for Physical Device
     private static Retrofit retrofit = null;
 
     public static ApiService getApiService() {
